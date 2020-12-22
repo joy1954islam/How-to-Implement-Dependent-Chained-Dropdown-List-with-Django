577f63bbe7e0ed92538e6a66caa129aa55485e7b
joyislam1954 <joyislam1954@gmail.com>
2020-12-22 12:24:10 +0600

    Django Dependent DropDown

--- django_dependent_dropdown/persons/urls.py
+++ django_dependent_dropdown/persons/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('add/', views.PersonCreateView.as_view(), name='person_add'),
     path('<int:pk>/', views.PersonUpdateView.as_view(), name='person_change'),
     path('ajax/load-cities/', views.load_cities, name='ajax_load_cities'),
+
+    # path('', views.PersonListView.as_view(), name='person_changelist'),
+    # path('add/', views.PersonCreateView, name='person_add'),
+    # path('<int:pk>/', views.PersonUpdateView, name='person_change'),
+    # path('ajax/load-cities/', views.load_cities, name='ajax_load_cities'),
+
 ]
--- django_dependent_dropdown/persons/views.py
+++ django_dependent_dropdown/persons/views.py
@@ -27,3 +27,40 @@ def load_cities(request):
     country_id = request.GET.get('country')
     cities = City.objects.filter(country_id=country_id).order_by('name')
     return render(request, 'persons/city_dropdown_list_options.html', {'cities': cities})
+
+
+
+#this is function base django dependent dropdown
+from django.http import JsonResponse
+# from django.shortcuts import render, redirect, get_object_or_404
+#
+# from .forms import PersonForm
+# from .models import Person, City
+#
+#
+# def PersonCreateView(request):
+#     form = PersonForm()
+#     if request.method == 'POST':
+#         form = PersonForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('person_add')
+#     return render(request, 'persons/home.html', {'form': form})
+#
+#
+# def PersonUpdateView(request, pk):
+#     person = get_object_or_404(Person, pk=pk)
+#     form = PersonForm(instance=person)
+#     if request.method == 'POST':
+#         form = PersonForm(request.POST, instance=person)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('person_change', pk=pk)
+#     return render(request, 'persons/home.html', {'form': form})
+#
+#
+# # AJAX
+# def load_cities(request):
+#     country_id = request.GET.get('country_id')
+#     cities = City.objects.filter(country_id=country_id).all()
+#     return render(request, 'persons/city_dropdown_list_options.html', {'cities': cities})
